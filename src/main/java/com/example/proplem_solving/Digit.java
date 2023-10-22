9fdc57b5d184e35fc55cc5e2208b23055ea9d948
ma7moudshoman <shomanm53@gmail.com>
2023-10-23 01:23:53 +0300

    Digit

---
+++ src/main/java/com/example/proplem_solving/Digit.java
@@ -0,0 +1,24 @@
+package com.example.proplem_solving;
+
+import java.util.Scanner;
+
+public class Digit {
+    public static void main(String[]args){
+        System.out.println("Enter Your Number");
+
+        Scanner scanner=new Scanner(System.in);
+        int value;
+        int size;
+        value=scanner.nextInt();
+        size=scanner.nextInt();
+        for (int i =0 ; i<size;i++){
+            while (value != 0){
+                int number= value %10;
+                System.out.print(number + " ");
+                value/=10;
+            }
+            System.out.println();
+        }
+
+    }
+}
--- src/main/java/com/example/proplem_solving/problemSolving.java
+++ src/main/java/com/example/proplem_solving/problemSolving.java
@@ -1,30 +1,8 @@
 package com.example.proplem_solving;
 
 
-import java.util.Scanner;
 
 public class problemSolving {
     public static void main(String[] args) {
-        System.out.println("enter your number");
-       /* Scanner input=new Scanner(System.in);
-
-  long y =input.nextInt();
-
-   if (y ==1){
-        System.out.println("No");}
-else
-       System.out.println("Yes");}
-}*/
-
-        // or
-        Scanner input = new Scanner(System.in);
-
-        long num = input.nextInt();
-       long res = 1;
-        for (long i = 1; i <= num; i++) {
-            res *= i;
-        }
-
-            System.out.println( "res :  " + res);
-        }
+         }
     }
