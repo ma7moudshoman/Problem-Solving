7af2afa5f38af778fe0bbd61acf2c61fb826f2f1
ma7moudshoman <shomanm53@gmail.com>
2023-11-06 13:01:30 +0200

    CoordinatesofaPoint

---
+++ src/main/java/com/example/proplem_solving/FirstDigit.java
@@ -0,0 +1,23 @@
+package com.example.proplem_solving;
+
+import java.util.Scanner;
+
+public class FirstDigit {
+    public  static void main(String[]args){
+
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter your number");
+
+        int num ;
+        num=input.nextInt();
+ int result= num/1000;              // any number abstract (1000) will give me the first number  like (4569/1000 )= (4)
+        System.out.println(result);
+        if (result%2==0){
+            System.out.println("EVEN NUMBER");
+        }
+        else
+            System.out.println("ODD NUMBER");
+
+    }
+
+}
